e239bacefb7c0c5f51e0cf9500f499de514b1d9d
Ashish Shukla <ashishfun008@gmail.com>
2022-03-16 00:29:11 +0530

    added safe search options

--- src/features/photos/photosAPI.ts
+++ src/features/photos/photosAPI.ts
@@ -7,7 +7,7 @@ export function getRecentPhotos(
   page: number
 ) {
   return axios.get(
-    `${BASE_URL}/?method=flickr.photos.getRecent&api_key=${api_key}&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`
+    `${BASE_URL}/?method=flickr.photos.getRecent&api_key=${api_key}&safe_search=1&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`
   );
 }
 
@@ -18,6 +18,6 @@ export function getPhotoBySearchText(
   searchText: string
 ) {
   return axios.get(
-    `${BASE_URL}/?method=flickr.photos.search&api_key=${api_key}&text=${searchText}&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`
+    `${BASE_URL}/?method=flickr.photos.search&api_key=${api_key}&safe_search=1&text=${searchText}&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`
   );
 }
